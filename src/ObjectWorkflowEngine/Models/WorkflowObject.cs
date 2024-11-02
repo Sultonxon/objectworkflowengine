e18cb0f62345a20e186b09545f7c9ecfff120148
WIN-0DDNL527R7H\User <qudratovsultonxon20011124@gmaIL.COM>
2024-11-02 12:26:23 +0500

    Add workflow object

---
+++ src/ObjectWorkflowEngine/Models/WorkflowObject.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1.Models
+{
+    public class WorkflowObject
+    {
+        public long Id { get; set; }
+        public long ObjectId { get; set; }
+        public int ObjectTypeId { get; set; }
+        
+    }
+}
